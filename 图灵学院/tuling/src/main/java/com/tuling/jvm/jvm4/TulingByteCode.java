03c2a4c27f459672b259986615fd7c2f56e9a172
BitbucketFeiLong <1696741038@qq.com>
2021-07-06 18:09:35 +0800

    jvm

---
+++ 图灵学院/tuling/src/main/java/com/tuling/jvm/jvm4/TulingByteCode.java
@@ -0,0 +1,19 @@
+package com.tuling.jvm.jvm4;
+
+/**
+ * @author e-Feilong.Chen
+ * @version 1.0
+ * @description TODO
+ * @date 2021/7/6 16:11
+ */
+public class TulingByteCode {
+    private String userName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
